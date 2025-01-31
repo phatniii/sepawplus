da749c03218a33a6fdd9c96355dca764cddc4188
phatni <phattanisonk65@nu.ac.th>
2025-01-31 21:17:49 +0700

    cm

--- src/utils/apiLineReply.ts
+++ src/utils/apiLineReply.ts
@@ -982,93 +982,98 @@ export const replyNotificationPostback = async ({
     type,
     message,
     replyToken
-}: ReplyNotificationPostback) => {
+}: ReplyNotificationPostback ) => {
     try {
+
         const requestData = {
-            replyToken,
+            to:replyToken,
             messages: [
                 {
-                    type: "flex",
-                    altText: "แจ้งเตือน",
+                    type    : "flex",
+                    altText : "แจ้งเตือน",
                     contents: {
                         type: "bubble",
                         body: {
-                            type: "box",
-                            layout: "vertical",
+                            type    : "box",
+                            layout  : "vertical",
                             contents: [
                                 {
-                                    type: "text",
-                                    text: " ",
+                                    type    : "text",
+                                    text    : " ",
                                     contents: [
                                         {
-                                            type: "span",
-                                            text: "แจ้งเตือนเขตปลอดภัย",
-                                            color: "#FC0303",
-                                            size: "xl",
-                                            weight: "bold",
+                                            type      : "span",
+                                            text      : "แจ้งเตือนเขตปลอดภัย",
+                                            color     : "#FC0303",
+                                            size      : "xl",
+                                            weight    : "bold",
                                             decoration: "none"
                                         },
                                         {
-                                            type: "span",
-                                            text: " ",
-                                            size: "xxl",
+                                            type      : "span",
+                                            text      : " ",
+                                            size      : "xxl",
                                             decoration: "none"
                                         }
                                     ]
                                 },
                                 {
-                                    type: "separator",
+                                    type  : "separator",
                                     margin: "md"
                                 },
                                 {
-                                    type: "text",
-                                    text: " ",
-                                    wrap: true,
+                                    type  : "text",
+                                    text  : " ",
+                                    wrap : true,
                                     lineSpacing: "5px",
                                     margin: "md",
-                                    contents: [
+                                    contents:[
                                         {
-                                            type: "span",
-                                            text: message,
-                                            color: "#555555",
-                                            size: "md"
+                                            type      : "span",
+                                            text      : message,
+                                            color     : "#555555",
+                                            size      : "md",
+                                            // decoration: "none",
+                                            // wrap      : true
                                         },
                                         {
-                                            type: "span",
-                                            text: " ",
-                                            size: "xl",
+                                            type      : "span",
+                                            text      : " ",
+                                            size      : "xl",
                                             decoration: "none"
                                         }
                                     ]
                                 },
                                 {
-                                    type: "button",
-                                    style: "primary",
+                                    type  : "button",
+                                    style : "primary",
                                     height: "sm",
                                     margin: "xxl",
                                     action: {
-                                        type: "postback",
+                                        type : "postback",
                                         label: "ส่งความช่วยเหลือเพิ่มเติม",
-                                        data: `userLineId=${userId}&takecarepersonId=${takecarepersonId}&type=${type}`,
+                                        data : `userLineId=${replyToken}&takecarepersonId=${takecarepersonId}&type=${type}`,
                                     }
                                 },
                                 {
-                                    type: "text",
-                                    text: " ",
-                                    wrap: true,
+                                    type  : "text",
+                                    text  : " ",
+                                    wrap : true,
                                     lineSpacing: "5px",
                                     margin: "md",
-                                    contents: [
+                                    contents:[
                                         {
-                                            type: "span",
-                                            text: "*หมาย: ข้าพเจ้ายินยอมเปิดเผยข้อมูลตำแหน่งปัจจุบันของผู้สูงอายุ",
-                                            color: "#FC0303",
-                                            size: "md"
+                                            type      : "span",
+                                            text      : "*หมาย: ข้าพเจ้ายินยอมเปิดเผยข้อมูลตำแหน่งปัจจุบันของผู้สูงอายุ",
+                                            color     : "#FC0303",
+                                            size      : "md",
+                                            // decoration: "none",
+                                            // wrap      : true
                                         },
                                         {
-                                            type: "span",
-                                            text: " ",
-                                            size: "xl",
+                                            type      : "span",
+                                            text      : " ",
+                                            size      : "xl",
                                             decoration: "none"
                                         }
                                     ]
@@ -1079,13 +1084,13 @@ export const replyNotificationPostback = async ({
                 }
             ],
         };
-        await axios.post(LINE_PUSH_MESSAGING_API, requestData, { headers: LINE_HEADER });
+       await axios.post(LINE_PUSH_MESSAGING_API, requestData, { headers:LINE_HEADER });
     } catch (error) {
         if (error instanceof Error) {
             console.log(error.message);
         }
     }
-};
+}
 
 export const replyNotificationSOS = async ({
     replyToken,
