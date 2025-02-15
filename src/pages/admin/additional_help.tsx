e693a4274d193d3855c93436fc4a68a6144879c5
phatni <phattanisonk65@nu.ac.th>
2025-02-15 12:28:38 +0700

    editadditional

--- src/pages/admin/additional_help.tsx
+++ src/pages/admin/additional_help.tsx
@@ -55,7 +55,7 @@ const AdditionalHelp = () => {
                    return moment(item.exten_date).format('M')
                 })
                 const exted_closed = _.groupBy(response.data, (item) => {
-                    return moment(item.exted_closed).format('M') && item.exted_closed_date
+                    return moment(item.exted_closed_date).format('M') 
                 })
                 const labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
                 const data = {
