7b16503b9597395a14d2fcdd6e9ef324a7d804b9
phatniii <phattanisonk65@nu.ac.th>
2025-06-03 11:08:31 +0700

    addsettingTemp

--- src/utils/apiLineReply.ts
+++ src/utils/apiLineReply.ts
@@ -661,6 +661,17 @@ export const replySetting = async ({
                                         uri  : `${WEB_API}/setting?auToken=${userData.users_line_id}&idsafezone=${idsafezone}`
                                     }
                                 },
+                                {
+                                    type : "button",
+                                    style :"primary",
+                                    height : "sm",
+                                    margin : "xxl",
+                                    action:{
+                                        type :"uri",
+                                        label : "ตั้งค่าอุณหภูมิ",
+                                        //uri :`${WEB_API}/setting?auToken=${userData.users_line_id}&idsafezone=${idsafezone}`
+                                    }
+                                }
                             ]
                         }
                     }
