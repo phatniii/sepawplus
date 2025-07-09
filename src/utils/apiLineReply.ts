9c465c7865d658faabf2365ee539028fb5843898
phatniii <phattanisonk65@nu.ac.th>
2025-07-09 10:53:58 +0700

    feat: add heart rate min/max setting and UI

--- src/utils/apiLineReply.ts
+++ src/utils/apiLineReply.ts
@@ -753,7 +753,7 @@ export const replySetting = async ({
                   action: {
                     type: "uri",
                     label: "ตั้งค่าอัตราการเต้นหัวใจ",
-                    uri: `${WEB_API}/settingHeartrate?auToken=${userData.users_line_id}&idsetting=${idSettingHR || ''}`
+                    uri: `${WEB_API}/settingHeartRate?auToken=${userData.users_line_id}&idsetting=${idSettingHR || ''}`
                   }
                 }
               ]
